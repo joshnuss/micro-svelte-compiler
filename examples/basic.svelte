fd826ea33bbe7500c4bef3741c459744eec12f61
Josh Nussbaum <joshnuss@gmail.com>
2019-11-30 03:52:25 -0500

    Adds example file

---
+++ examples/basic.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let name, foo;
+
+  function handleClick(e) {
+    e.preventDefault()
+    alert(`Hello ${name}!`)
+  }
+</script>
+
+<h1 class="foo" on:click=handleClick>Hello {name}!</h1>
